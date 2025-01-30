2439e227cf2a496c61ca4ac68e3f7c3d2c29b2d6
Josue Gatica Odato <josuegaticaodato@gmail.com>
2025-01-30 05:31:26 -0300

    Clases Carta, Mazo y Palo creadas

---
+++ src/ar/edu/unlu/corazones/modelo/Carta.java
@@ -0,0 +1,96 @@
+package ar.edu.unlu.corazones.modelo;
+
+import java.io.Serializable;
+
+public class Carta implements Serializable{
+	
+	// *************************************************************
+	//                        SERIALIZABLE
+	// *************************************************************
+	
+	private static final long serialVersionUID = 1L;
+	
+	// *************************************************************
+	//                        ATRIBUTOS
+	// *************************************************************
+	
+	private Palo palo;
+	
+	private int valor;
+	
+	private String valorTexto; // Numeros o  J,Q,K y A
+	
+	// *************************************************************
+	//                       CONSTRUCTOR
+	// *************************************************************
+	
+	public Carta(Palo palo, int valor) {
+		setPalo(palo);
+		setValor(valor);
+		valorCarta();
+	}
+	
+	// *************************************************************
+	//                       COMPORTAMIENTO
+	// *************************************************************
+
+	public String mostrarCarta() {
+			return valorTexto + " - " + palo.toString();
+	}
+	
+	private void valorCarta() {
+		switch (this.valor) {
+		case 11:
+			setValorTexto("J");
+			break;
+		case 12:
+			setValorTexto("Q");
+			break;
+		case 13:
+			setValorTexto("K");
+			break;
+		case 1:
+			setValorTexto("A");
+			setValor(14); //La A es el valor mas alto en el juego
+			break;
+		default:
+			setValorTexto(String.valueOf(this.valor));
+			break;
+		}
+	}
+	
+	// *************************************************************
+	//                      	GETTERS
+	// *************************************************************
+	
+	public Palo getPalo() {
+		return palo;
+	}
+
+	public int getValor() {
+		return valor;
+	}
+	
+	public String getValorTexto() {
+		return valorTexto;
+	}
+	
+	// *************************************************************
+	//                      	SETTERS
+	// *************************************************************
+	
+	public void setPalo(Palo palo){
+		this.palo = palo;
+	}
+	
+	public void setValor(int valor){
+		this.valor = valor;
+	}
+	
+	public void setValorTexto(String valorT) {
+		this.valorTexto = valorT;
+	}
+	
+}
+
+
---
+++ src/ar/edu/unlu/corazones/modelo/Palo.java
@@ -0,0 +1,5 @@
+package ar.edu.unlu.corazones.modelo;
+
+public enum Palo {
+	PICAS, DIAMANTE, CORAZONES, TREBOL
+}

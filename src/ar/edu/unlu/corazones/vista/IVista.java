6912da10a3b154d5d808eb8ad2af6551e9ed0c04
Josue Gatica Odato <josuegaticaodato@gmail.com>
2025-02-13 19:41:06 -0300

    Vista Consola creada

---
+++ src/ar/edu/unlu/corazones/vista/IVista.java
@@ -0,0 +1,61 @@
+package ar.edu.unlu.corazones.vista;
+
+import ar.edu.unlu.corazones.controlador.Controlador;
+
+public interface IVista {
+
+	// *************************************************************
+	//                         PRE-JUEGO
+	// *************************************************************
+	
+	void iniciar();
+	
+	// *************************************************************
+    //                      PASAJE DE CARTAS
+	// *************************************************************
+	
+	void pasajeDeCartas();
+	
+	void pedirCartaPasaje();
+	
+	void cartaTiradaInvalidaPasaje();
+	
+	void cartaTiradaValidaPasaje();
+	
+	void finPasajeDeCartas();
+
+	// *************************************************************
+	//                          JUEGO
+	// *************************************************************
+	
+	void cartasRepartidas();
+
+	void pedirCarta();
+	
+	void jugarDosDeTrebol();
+
+	void cartaTiradaInvalida();
+	
+	void cartaTiradaInvalida2deTrebol();
+	
+	void perdedorJugada();
+	
+	void corazonesRotos();
+	
+	void cartaTiradaValida();
+	
+	// *************************************************************
+	//                    FIN JUEGO - RONDA
+	// *************************************************************
+	
+	void finDeRonda();
+
+	void finDeJuego();
+	
+	// *************************************************************
+	//                		 OBSERVER
+	// *************************************************************
+	
+	void setControlador(Controlador controlador);
+
+}
